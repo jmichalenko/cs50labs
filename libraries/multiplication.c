a5cc44b5ad0dd8d07de3b81e3b5e6e65ec52a277
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2022-04-08 19:07:17 -0400

    Update multiplication.c

--- libraries/multiplication.c
+++ libraries/multiplication.c
@@ -3,8 +3,8 @@
 
 int main (void)
 {
-  int numberA = get_int("Enter number A:  ");
-  int numberB = get_int("Enter number B:  ");
+  int numberA = get_int("Enter number A:  \n");
+  int numberB = get_int("Enter number B:  \n");
   
   printf("The product is: %i\n", numberA * numberB);
 }
